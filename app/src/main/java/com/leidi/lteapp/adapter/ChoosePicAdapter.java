385c21c1030efccd197e956ef1584e4ecbf4b29e
阎国庆 <414497786@qq.com>
2022-11-22 09:12:55 +0800

    例行提交

--- app/src/main/java/com/leidi/lteapp/adapter/ChoosePicAdapter.java
+++ app/src/main/java/com/leidi/lteapp/adapter/ChoosePicAdapter.java
@@ -10,14 +10,10 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.leidi.lteapp.R;
-import com.nanchen.compresshelper.CompressHelper;
 import com.squareup.picasso.MemoryPolicy;
 import com.squareup.picasso.Picasso;
-import com.zhihu.matisse.engine.impl.GlideEngine;
 
-import java.io.File;
 import java.util.List;
 
 /**
@@ -68,8 +64,6 @@ public class ChoosePicAdapter extends BaseAdapter {
         if ("占位图".equals(mPaths.get(position).trim())) {
             Picasso.get().load(R.mipmap.act_send_detail_add_last).memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE).into(holder.itemImage);
         } else {
-//            File newFile = CompressHelper.getDefault(context).compressToFile(new File(mPaths.get(position).trim()));
-            //防止有些图加载不出来显示白块[用上面的代码压缩之后再用框架加载file会有几率导致第一张图显示白块]
             if (mPaths.get(position).trim().endsWith(".mp4")) {
                 Glide.with(context).load(mPaths.get(position)).placeholder(R.mipmap.act_send_detail_add_last).into(holder.itemImage);
             } else {
@@ -81,7 +75,6 @@ public class ChoosePicAdapter extends BaseAdapter {
                         .memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE)
                         .config(Bitmap.Config.RGB_565)
                         .into(holder.itemImage);
-
             }
         }
         return view;
